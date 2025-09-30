8a61884521ea696e482d0e00a0deefc48c3f4fad
mypc <mashanto09@gmail.com>
2025-09-30 19:42:05 +0600

    add a loop

--- gg.py
+++ gg.py
@@ -1,7 +1,9 @@
 print("hi my name is shanto.",end=(" "))
 print("and i am a student")
 print("evrerything is good")
-i=1
+"""i=1
 while i<=100:
     print(i)
-    i+=1
+    i+=1"""
+for i in range(3,40,3):
+    print(i+1)
