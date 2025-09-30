4c8d3fc23cc743eb66c8be7284cd59657ed368db
mypc <mashanto09@gmail.com>
2025-09-30 19:51:11 +0600

    add a new txt

--- gg.py
+++ gg.py
@@ -1,9 +1,5 @@
 print("hi my name is shanto.",end=(" "))
 print("and i am a student")
 print("evrerything is good")
-"""i=1
-while i<=100:
-    print(i)
-    i+=1"""
 for i in range(3,40,3):
     print(i+1)
